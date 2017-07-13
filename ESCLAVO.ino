66b2adc3c618a85a53a322989ccae5ce8eea206c
kervins7 <liverpolkervins2017@gmail.com>
2017-07-13 07:50:25 +0200

    Add files via upload

---
+++ ESCLAVO.ino
@@ -0,0 +1,83 @@
+#include <SoftwareSerial.h>
+#include <AFMotor.h>
+#include <Servo.h>
+SoftwareSerial BT(2, 3); //Rx,Tx
+AF_DCMotor motorA(1);
+Servo servo1;
+
+void setup() {
+   servo1.attach(10,600,1500);
+  Serial.begin(9600);
+  BT.begin(9600);
+motorA.setSpeed(250);
+}
+
+void moverDerecha(){
+ 
+}
+
+void moverIzquierda(){
+
+}
+
+void moverAdelante(){
+  motorA.run(FORWARD);
+}
+
+void moverAtras(){
+  motorA.run(BACKWARD);
+}
+
+void parar(){
+motorA.run(RELEASE);  
+}
+
+void loop() {
+  
+  while (BT.available() > 0) {
+    char dataIn = BT.read();
+    
+    if ( dataIn == 'i') {
+      servo1.write(0);
+      delay(15);
+ 
+     }
+    
+    else if ( dataIn == 'b') {
+      moverAtras();
+      servo1.write(90);
+      delay(15);
+    }
+    
+    else if (dataIn == 'd') {
+     servo1.write(180);
+     delay(15);
+    }
+     
+    else if ( dataIn == 'a') {
+      moverAdelante();
+      servo1.write(90);
+      delay(15);
+    }
+
+    
+    else if ( dataIn == 'x') {
+      parar();
+      servo1.write(90);
+      delay(15);
+    }
+
+    else if ( dataIn == 'y') {
+      parar();
+      servo1.write(90);
+      delay(15);
+    }
+ 
+   else {
+      parar();
+      servo1.write(90);
+      delay(15);
+    }
+  }
+}
+
---
+++ MAESTRO.ino
@@ -0,0 +1,46 @@
+#include <Esplora.h>
+#include <Mouse.h>
+#include<SoftwareSerial.h>
+
+SoftwareSerial BT(8,7);
+
+void setup() {
+  Serial.begin(9600);
+  BT.begin(9600);
+   Mouse.begin();
+}
+
+void loop() {
+   
+  int adelante=Esplora.readButton(SWITCH_UP);
+  int atras=Esplora.readButton(SWITCH_DOWN);
+ int xderecha = Esplora.readJoystickX();
+ int xizquierda=Esplora.readJoystickX();
+
+if(xderecha>300)
+{
+  BT.print("d");
+  }else if(xizquierda<-300)
+  {
+    BT.print("i");
+    } 
+    
+if (adelante==HIGH && atras==HIGH)
+  {
+    BT.print("x");
+  }
+else if (adelante==LOW && atras==LOW)
+  {
+    BT.print("y");
+  }
+else if (adelante==LOW && atras==HIGH)
+  {
+    BT.print("a");
+  }
+else if (adelante==HIGH  && atras==LOW)
+  {
+    BT.print("b");
+  }  
+ }
+
+
